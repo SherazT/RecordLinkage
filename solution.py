ebe515f83288fcce24bfe0b867e9b790684f3569
Sheraz Tariq <sheraztariq@hotmail.com>
2017-04-21 00:25:46 -0400

    fixed column size error

--- solution.py
+++ solution.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import numpy as np
 import csv
+import pdb #byebug for python --> pdb.set_trace()
+
+# with open('people.csv', newline='') as csvfile:
+# 	file = csv.reader(csvfile, delimiter=' ', quotechar='|')
+# 	for row in file:
+# 		pdb.set_trace()		
+
+df = pd.read_csv('test.csv',escapechar='\\', encoding='utf-8')
+
+pdb.set_trace()
