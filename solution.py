2cac1000e0df512712d309ffe6315fb93e71ed10
Sheraz Tariq <sheraztariq@hotmail.com>
2017-04-27 06:21:26 -0400

    returns all matching entities

--- solution.py
+++ solution.py
@@ -5,29 +5,42 @@ import recordlinkage
 import pdb #byebug for python --> pdb.set_trace()
 
 
+
+
 df = pd.read_csv('test.csv',escapechar='\\')
 
 df_1 = df[df.source == 'source_1']
 df_2 = df[df.source == 'source_2']
 df_3 = df[df.source == 'source_3']
 
-#-----------------------compare source 1 and source 2-----------------
-index = recordlinkage.Pairs(df_1, df_2)
-links = index.full()
-fill = recordlinkage.Compare(links, df_1, df_2)
+def remove_duplicates(values):
+    output = []
+    seen = set()
+    for value in values:
+        if value not in seen:
+            output.append(value)
+            seen.add(value)
+    return output
 
-fill.string('first_name', 'first_name', method='jarowinkler',threshold=0.70)
-fill.string('last_name', 'last_name', method='jarowinkler', threshold=0.70)
+def s1_s2():
+	#-----------------------compare source 1 and source 2-----------------
+	index = recordlinkage.Pairs(df_1, df_2)
+	links = index.full()
+	fill = recordlinkage.Compare(links, df_1, df_2)
 
-result = fill.vectors
-correct = result[(result[0] == 1) & (result[1] == 1)]
+	fill.string('first_name', 'first_name', method='jarowinkler',threshold=0.70)
+	fill.string('last_name', 'last_name', method='jarowinkler', threshold=0.70)
 
-first_df_index = correct.index.tolist()[0][0]
-second_df_index = correct.index.tolist()[0][1]
+	result = fill.vectors
+	correct = result[(result[0] == 1) & (result[1] == 1)]
 
+	first_df_index = remove_duplicates(correct.index.labels[0])
+	second_df_index = remove_duplicates(correct.index.labels[1])
 
-pdb.set_trace()
+	return df_1.iloc[first_df_index].append(df_2.iloc[second_df_index])
+	
 
+s1_s2()
 
 # if __name__ == "__main__":
 #     main()
