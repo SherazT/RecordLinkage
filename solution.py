2d726aab659d30e8cf91d2031f7eb25998a0bca8
Sheraz Tariq <sheraztariq@hotmail.com>
2017-04-27 07:45:10 -0400

    cleanup

--- solution.py
+++ solution.py
@@ -4,26 +4,16 @@ import jellyfish
 import recordlinkage
 import pdb #byebug for python --> pdb.set_trace()
 
-df = pd.read_csv('people.csv',escapechar='\\')
+df = pd.read_csv('test.csv',escapechar='\\')
 
 df_1 = df[df.source == 'source_1']
 df_2 = df[df.source == 'source_2']
 df_3 = df[df.source == 'source_3']
 
-def remove_duplicates(values):
-    output = []
-    seen = set()
-    for value in values:
-        if value not in seen:
-            output.append(value)
-            seen.add(value)
-    return output
-
-def s1_s2():
-	#-----------------------compare source 1 and source 2-----------------
-	index = recordlinkage.Pairs(df_1, df_2)
+def find_all_matches(first_df,second_df):
+	index = recordlinkage.Pairs(first_df, second_df)
 	links = index.full()
-	fill = recordlinkage.Compare(links, df_1, df_2)
+	fill = recordlinkage.Compare(links, first_df, second_df)
 
 	fill.string('first_name', 'first_name', method='jarowinkler',threshold=0.70)
 	fill.string('last_name', 'last_name', method='jarowinkler', threshold=0.70)
@@ -34,46 +24,16 @@ def s1_s2():
 	first_df_index = remove_duplicates(correct.index.labels[0])
 	second_df_index = remove_duplicates(correct.index.labels[1])
 
-	return df_1.iloc[first_df_index].append(df_2.iloc[second_df_index])
-	
-def s1_s3():
-	#-----------------------compare source 1 and source 3-----------------
-	index = recordlinkage.Pairs(df_1, df_3)
-	links = index.full()
-	fill = recordlinkage.Compare(links, df_1, df_3)
-
-	fill.string('first_name', 'first_name', method='jarowinkler',threshold=0.70)
-	fill.string('last_name', 'last_name', method='jarowinkler', threshold=0.70)
-
-	result = fill.vectors
-	correct = result[(result[0] == 1) & (result[1] == 1)]
-
-	first_df_index = remove_duplicates(correct.index.labels[0])
-	second_df_index = remove_duplicates(correct.index.labels[1])
-
-	return df_1.iloc[first_df_index].append(df_3.iloc[second_df_index])
-
-def s2_s3():
-	#-----------------------compare source 2 and source 3-----------------
-	index = recordlinkage.Pairs(df_2, df_3)
-	links = index.full()
-	fill = recordlinkage.Compare(links, df_2, df_3)
-
-	fill.string('first_name', 'first_name', method='jarowinkler',threshold=0.70)
-	fill.string('last_name', 'last_name', method='jarowinkler', threshold=0.70)
-
-	result = fill.vectors
-	correct = result[(result[0] == 1) & (result[1] == 1)]
-
-	first_df_index = remove_duplicates(correct.index.labels[0])
-	second_df_index = remove_duplicates(correct.index.labels[1])
-
-	return df_2.iloc[first_df_index].append(df_3.iloc[second_df_index])
+	return first_df.iloc[first_df_index].append(second_df.iloc[second_df_index])
 	
 
-final = s1_s2().append(s1_s3().append(s2_s3())).drop_duplicates()
+	
 
-pdb.set_trace()
+final = find_all_matches(df_1,df_2).append(find_all_matches(df_1,df_3).append(find_all_matches(df_2,df_3))).drop_duplicates()
+
+final.to_csv('results.csv')
+
+# pdb.set_trace()
 
 # if __name__ == "__main__":
 #     main()
