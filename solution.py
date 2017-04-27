15a2d9b92b690e74d4a30073ef9872a7d4fa9c87
Sheraz Tariq <sheraztariq@hotmail.com>
2017-04-27 05:54:36 -0400

    about to return matching strings

--- solution.py
+++ solution.py
@@ -2,24 +2,28 @@ import pandas as pd
 import numpy as np
 import jellyfish
 import recordlinkage
-from nameparser import HumanName
 import pdb #byebug for python --> pdb.set_trace()
 
 
 df = pd.read_csv('test.csv',escapechar='\\')
 
 df_1 = df[df.source == 'source_1']
-df_2 = df[df.source == 'source_3']
-# df_3 = df[df.source == 'source_3']
+df_2 = df[df.source == 'source_2']
+df_3 = df[df.source == 'source_3']
 
+#-----------------------compare source 1 and source 2-----------------
 index = recordlinkage.Pairs(df_1, df_2)
 links = index.full()
 fill = recordlinkage.Compare(links, df_1, df_2)
 
-fill.string('first_name', 'first_name', method='jarowinkler', threshold=0.65)
-fill.string('last_name', 'last_name', method='jarowinkler', threshold=0.65)
+fill.string('first_name', 'first_name', method='jarowinkler',threshold=0.70)
+fill.string('last_name', 'last_name', method='jarowinkler', threshold=0.70)
 
-print(fill.vectors.head())
+result = fill.vectors
+correct = result[(result[0] == 1) & (result[1] == 1)]
+
+first_df_index = correct.index.tolist()[0][0]
+second_df_index = correct.index.tolist()[0][1]
 
 
 pdb.set_trace()
