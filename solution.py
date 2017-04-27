d2eb9e3e3949fa4bdf787618da921fb058b4c3ec
Sheraz Tariq <sheraztariq@hotmail.com>
2017-04-27 04:52:20 -0400

    progress on pairing

--- solution.py
+++ solution.py
@@ -1,22 +1,32 @@
 import pandas as pd
 import numpy as np
+import jellyfish
 import recordlinkage
+from nameparser import HumanName
 import pdb #byebug for python --> pdb.set_trace()
 
-# with open('people.csv', newline='') as csvfile:
-# 	file = csv.reader(csvfile, delimiter=' ', quotechar='|')
-# 	for row in file:
-# 		pdb.set_trace()		
 
 df = pd.read_csv('test.csv',escapechar='\\')
 
-df_1 = df[df.source == ' source_1']
-df_2 = df[df.source == ' source_2']
-df_3 = df[df.source == ' source_3']
+df_1 = df[df.source == 'source_1']
+df_2 = df[df.source == 'source_2']
+df_3 = df[df.source == 'source_3']
 
-# index = recordlinkage.Index(df_1, df_2)
+index = recordlinkage.Pairs(df_1, df_2)
 
-# candidate_links = index.block('last_name')
+links = index.full()
+
+fill = recordlinkage.Compare(links, df_1, df_2)
+
+pdb.set_trace()
+
+
+fill.string('first_name', 'first_name', method='jarowinkler')
+fill.string('last_name', 'last_name', method='jarowinkler')
+
+
+ecm = recordlinkage.ECMClassifier()
+ecm.learn(fill.vectors)
 
 
 pdb.set_trace()
