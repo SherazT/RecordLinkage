cfaf96f45909cfdd1d3c38066754d4f3674c9dc5
Sheraz Tariq <sheraztariq@hotmail.com>
2017-04-28 06:20:41 -0400

    added ml model

--- solution.py
+++ solution.py
@@ -3,31 +3,43 @@ import timeit
 import numpy as np
 import jellyfish
 import recordlinkage
-import pdb #byebug for python --> pdb.set_trace()
+from recordlinkage.datasets import load_krebsregister
+import pdb 
 
-df = pd.read_csv('people.csv',escapechar='\\')
+df = pd.read_csv('cross.csv',escapechar='\\')
 
 df_1 = df[df.source == 'source_1']
 df_2 = df[df.source == 'source_2']
 df_3 = df[df.source == 'source_3']
 
-def find_all_matches(first_df,second_df):
+def find_all_matches(first_df, second_df):
 	index = recordlinkage.Pairs(first_df, second_df)
+	# links = index.random(n=0)
 	links = index.full()
 	fill = recordlinkage.Compare(links, first_df, second_df)
 
-	fill.string('first_name', 'first_name', method='jarowinkler',threshold=0.70)
-	fill.string('last_name', 'last_name', method='jarowinkler', threshold=0.70)
-
-	result = fill.vectors
-	correct = result[(result[0] == 1) & (result[1] == 1)]
+	fill.string('first_name', 'first_name', method='jaro') #threshold=0.72
+	fill.string('last_name', 'last_name', method='jaro') #threshold=0.82
+	comp_vector = ml_model(fill.vectors)
+	correct = comp_vector[(comp_vector[0] == 1) & (comp_vector[1] == 1)]
 	first_df_index = correct.index.labels[0]
 	second_df_index = correct.index.labels[1]
 
 	return first_df.iloc[first_df_index].append(second_df.iloc[second_df_index])
 
+def ml_model(comp_vector):
+	train_input = load_krebsregister(missing_values=0)
+	train_data = train_input[0][0:5000]
+	train_result = train_input[1][0:5000]
+
+	# intilize classifier
+	reg = recordlinkage.LogisticRegressionClassifier()
+	reg.learn(train_data, train_result)
+	
+	return reg.predict(comp_vector)
+
 def spit_out():
-	final = (find_all_matches(df_1,df_2).append(find_all_matches(df_1,df_3).append(find_all_matches(df_2,df_3)))).drop_duplicates()
+	final = (find_all_matches(df_1,df_2).append(find_all_matches(df_1,df_3).append(find_all_matches(df_2,df_3)))).drop_duplicates().sort('first_name')
 	final.to_csv('results.csv', sep='\t')
 
 # print(timeit.timeit(spit_out))
