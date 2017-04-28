8457db7f99c20d85dde02d603b27fef810dd7b34
Sheraz Tariq <sheraztariq@hotmail.com>
2017-04-28 10:00:39 -0400

    moved folders

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import jellyfish
 import recordlinkage
 import pdb #byebug for python --> pdb.set_trace()
 
-df = pd.read_csv('test.csv',escapechar='\\')
+df = pd.read_csv('data/test.csv',escapechar='\\')
 
 df_1 = df[df.source == 'source_1']
 df_2 = df[df.source == 'source_2']
@@ -18,17 +18,23 @@ def find_all_matches(first_df,second_df):
 
 	fill.string('first_name', 'first_name', method='jaro', threshold=0.70)
 	fill.string('last_name', 'last_name', method='jaro', threshold=0.83)
+
 	return get_df_from_vector(fill.vectors,first_df,second_df)
 
 def get_df_from_vector(result,first_df,second_df):
 	correct = result[(result[0] == 1) & (result[1] == 1)]
 	first_df_index = correct.index.labels[0]
 	second_df_index = correct.index.labels[1]
+
 	return first_df.iloc[first_df_index].append(second_df.iloc[second_df_index])
 
 def spit_out(first_df,second_df,third_df):
-	final_df = (find_all_matches(first_df,second_df).append(find_all_matches(first_df,third_df).append(find_all_matches(second_df,third_df)))).drop_duplicates().sort('first_name')
+	final_df = (find_all_matches(
+	first_df,second_df).append(find_all_matches(
+	first_df,third_df).append(find_all_matches(
+	second_df,third_df)))).drop_duplicates().sort('first_name')
+
 	return final_df
 
 final = spit_out(df_1,df_2,df_3)
-final.to_csv('results.csv', sep='\t')
+final.to_csv('data/results.csv', sep='\t')
--- test_solution.py
+++ test_solution.py
@@ -1,5 +1,19 @@
 import solution
+import pandas as pd
+import numpy as np
+import pdb
 
-def test_file():
-	assert solution.file() == 1
+df = pd.read_csv('data/test.csv',escapechar='\t')
+
+df_1 = df[df.source == 'source_1']
+df_2 = df[df.source == 'source_2']
+df_3 = df[df.source == 'source_3']
+
+expected_results = pd.read_csv('expected_results.csv',sep=',')
+
+def test_spit_out():
+	result = solution.spit_out(df_1,df_2,df_3)
+	pdb.set_trace()
+
+
 
